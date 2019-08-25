54db3e0cc80ff3fb40c45eb1a62b6e028278b09e
Priya Gupta <guptapriyab@gmail.com>
2019-08-25 22:35:54 +0200

    made app as functional component

--- App.js
+++ App.js
@@ -1,27 +1,19 @@
-import React,{Component} from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component{
-    state ={
-        persons:[{
-            name:'Priya',
-            age :25
-        }]
+const App = props =>{
+    const [personState,setPersonSate] = useState({persons:[{name:'Priya',age:25}]});
+    const [otherState,setOtherState] = useState('some other value');
+    const switchNameHandler = () =>{
+        setPersonSate({persons:[{name:'Aman',age:25}]});
     }
-    switchNameHandler=()=>{
-        this.setState({persons:[{name:'Aman',age:25}]});
-    }
-  render(){
-    return(
-        <div className="App">
-          <h1>I am React App</h1>
-            <button onClick = {this.switchNameHandler}>switch name</button>
-          <Person age = {this.state.persons[0].age} name ={this.state.persons[0].name}/>
 
-          </div>
-    )
+    return (<div className="App">
+          <h1>I am React App</h1>
+            <button onClick = {switchNameHandler}>switch name</button>
+          <Person age = {personState.persons[0].age} name ={personState.persons[0].name}/>
+        </div>)
   }
-}
 
 export default App;
