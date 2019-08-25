12799781bc5ba1a7e77a5111fb0eb5f7ee4f6420
Priya Gupta <guptapriyab@gmail.com>
2019-08-25 22:57:38 +0200

    added app.js changes

--- App.js
+++ App.js
@@ -5,14 +5,14 @@ import Person from './Person/Person';
 const App = props =>{
     const [personState,setPersonSate] = useState({persons:[{name:'Priya',age:25}]});
     const [otherState,setOtherState] = useState('some other value');
-    const switchNameHandler = () =>{
-        setPersonSate({persons:[{name:'Aman',age:25}]});
+    const switchNameHandler = (newName) =>{
+        setPersonSate({persons:[{name:newName,age:25}]});
     }
 
     return (<div className="App">
           <h1>I am React App</h1>
             <button onClick = {switchNameHandler}>switch name</button>
-          <Person age = {personState.persons[0].age} name ={personState.persons[0].name}/>
+          <Person age = {personState.persons[0].age} name ={personState.persons[0].name} click={switchNameHandler.bind(this,'Sourabh')}/>
         </div>)
   }
 
