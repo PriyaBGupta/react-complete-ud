ba9d980db9a6e184728d572306229b12fce1e5ac
Priya Gupta <guptapriyab@gmail.com>
2019-09-09 00:06:49 +0200

    removed traces of radium and worked upon css module

---
+++ App.js
@@ -0,0 +1,71 @@
+import React,{Component} from 'react';
+import classes from './App.module.css';
+import Person from './Person/Person';
+
+    class App extends Component{
+    state ={
+        persons:[{name:'Priya',age:25,id:'abc'},{name:'Aman',age:25,id:'xyz'},{name:'Vidya',age:30,id:'mno'}],
+        showPerson: false
+    }
+    switchNameHandler = (newName) =>{
+        console.log(newName);
+        this.setState({persons:[{name:newName,age:25}]});
+    }
+    togglePersonHandler = () =>{
+        const  doesShow = this.state.showPerson;
+        this.setState({showPerson:!doesShow});
+    }
+    deletePersonHandler = personIndex =>{
+        const persons = [...this.state.persons];
+        persons.splice(personIndex,1);
+        this.setState({persons:persons})
+    }
+    nameChangeHandler =(event,id)=>{
+        const personIndex = this.state.persons.findIndex(p=>{return p.id===id});
+        const person = {...this.state.persons[personIndex]};
+        person.name = event.target.value;
+        const persons =[...this.state.persons];
+        persons[personIndex] = person;
+        this.setState({persons:persons});
+    }
+
+    render (){
+        const style ={
+            backgroundColor:'blue',
+            fontSize:'20px'
+        }
+        let persons = null;
+        if(this.state.showPerson){
+            console.log("Is it working");
+            persons=(<div>
+                {this.state.persons.map((person,index)=>{
+                    return <Person
+                        name ={person.name}
+                        age={person.age}
+                        click ={()=>this.deletePersonHandler(index)}
+                        changed = {(event)=>this.nameChangeHandler(event,person.id)}
+                        key={person.id}/>
+                })}
+            </div>)
+            style.backgroundColor='red';
+
+        }
+        const assignedClasses =[];
+        if(this.state.persons.length<=2){
+            assignedClasses.push(classes['color-green']);
+        }
+        if(this.state.persons.length<=1){
+            assignedClasses.push(classes.bold);
+        }
+
+    return(
+        <div className={classes.App}>
+            <h1>I am React App</h1>
+            <p className={assignedClasses.join(' ')}>Heyy I am working on classes</p>
+            <button style ={style} onClick={this.togglePersonHandler}>togglePerson</button>
+            {persons}
+        </div>)
+    }
+}
+
+export default App;
---
+++ Person.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const person = (props) =>{
+    return (<div>
+        <p onClick={props.click}> I am {props.name} and I am {props.age} years old!</p>
+        <p>{props.children}</p>
+        <input type="text" onChange={props.changed} value={props.name}/>
+    </div>)
+}
+export default person;
+//Add a root elememt and wrap the transcluding element into that root element
