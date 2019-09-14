b554a130e02fdd9f9cd6470fd4d609dca25ee251
Priya Gupta <guptapriyab@gmail.com>
2019-09-14 19:29:58 +0200

    Created Cockpit

---
+++ components/Cockpit/Cockpit.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import classes from './Cockpit.module.css';
+const cockpit = (props)=>{
+    let style = classes['blue-button'];
+    const assignedClasses =[];
+    if(props.persons.length<=2){
+        assignedClasses.push(classes['color-green']);
+    }
+    if(props.persons.length<=1){
+        assignedClasses.push(classes.bold);
+    }
+    if(props.showPerson){
+        style = classes['red-button'];
+    }
+    return(
+        <div className={classes.Cockpit}>
+            <h1>PI am React App</h1>
+            <p className={assignedClasses.join(' ')}>Heyy I am working on classes</p>
+            <button className={style} onClick={props.toggle}>togglePerson</button>
+        </div>
+    )
+}
+export default cockpit;
