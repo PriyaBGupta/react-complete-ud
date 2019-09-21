e3dc12fae0b8750467657d5e45f3095b24c1d0be
Priya Gupta <guptapriyab@gmail.com>
2019-09-21 15:32:50 +0200

    rendered Person component as an array

--- components/Persons/Person/Person.js
+++ components/Persons/Person/Person.js
@@ -1,10 +1,13 @@
-import React from 'react';
-const person = (props) =>{
-    return (<div>
-        <p onClick={props.click}> I am {props.name} and I am {props.age} years old!</p>
-        <p>{props.children}</p>
-        <input type="text" onChange={props.changed} value={props.name}/>
-    </div>)
+import React,{Component} from 'react';
+class Person extends Component{
+
+    render(){
+        console.log('[Person.js] render');
+        return [
+            <p onClick={this.props.click} key='i1'> I am {this.props.name} and I am {this.props.age} years old!</p>,
+            <p key='i2'>{this.props.children}</p>,
+            <input key='i3' type="text" onChange={this.props.changed} value={this.props.name}/>]
+    }
 }
-export default person;
+export default Person;
 //Add a root elememt and wrap the transcluding element into that root element
