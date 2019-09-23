507e20ad009bd130d86f669b30fe5beded421c0b
Priya Gupta <guptapriyab@gmail.com>
2019-09-24 00:10:21 +0200

    Another form of HOC

--- container/App.js
+++ container/App.js
@@ -2,7 +2,8 @@ import React,{Component} from 'react';
 import classes from './App.module.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-
+import Auxilary from '../hoc/Auxilary';
+import withClass from '../hoc/withClass';
     class App extends Component{
         constructor(props){
             super(props);
@@ -15,11 +16,19 @@ import Cockpit from '../components/Cockpit/Cockpit';
         componentDidMount() {
             console.log('[App.js],componentDidMount ');
         }
+        shouldComponentUpdate(nextProps, nextState, nextContext) {
+            console.log('[App.js] shouldComponentUpdate');
+            return true;
+        }
+        componentDidUpdate() {
+            console.log('[App.js] componentDidUpdate');
+        }
 
         state ={
         persons:[{name:'Priya',age:25,id:'abc'},{name:'Aman',age:25,id:'xyz'},{name:'Vidya',age:30,id:'mno'}],
-        showPerson: false
-    }
+        showPerson: false,
+        showCockpit:true
+        }
     switchNameHandler = (newName) =>{
         console.log(newName);
         this.setState({persons:[{name:newName,age:25}]});
@@ -52,20 +61,21 @@ import Cockpit from '../components/Cockpit/Cockpit';
                 clicked={this.deletePersonHandler}
                 changed={this.nameChangeHandler}
             />
-
-
         }
 
 
     return(
-        <div className={classes.App}>
-            <Cockpit
-                persons={this.state.persons}
+        <Auxilary>
+
+            <button onClick={()=>{this.setState({showCockpit:false})}}>Remove cockpit</button>
+            {this.state.showCockpit?<Cockpit
+                personsLength={this.state.persons.length}
                 showPerson ={this.state.showPerson}
-                toggle ={this.togglePersonHandler}/>
+                toggle ={this.togglePersonHandler}/>:null}
             {persons}
-        </div>)
+        </Auxilary>
+    )
     }
 }
 
-export default App;
+export default withClass(App,classes.App);
