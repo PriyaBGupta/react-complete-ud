1e7c183a548397650ff36fd2e1ee0921b8363211
Priya Gupta <guptapriyab@gmail.com>
2019-09-25 23:57:01 +0200

    setState updated

--- container/App.js
+++ container/App.js
@@ -2,7 +2,8 @@ import React,{Component} from 'react';
 import classes from './App.module.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-
+import Auxilary from '../hoc/Auxilary';
+import withClass from '../hoc/withClass';
     class App extends Component{
         constructor(props){
             super(props);
@@ -15,11 +16,20 @@ import Cockpit from '../components/Cockpit/Cockpit';
         componentDidMount() {
             console.log('[App.js],componentDidMount ');
         }
+        shouldComponentUpdate(nextProps, nextState, nextContext) {
+            console.log('[App.js] shouldComponentUpdate');
+            return true;
+        }
+        componentDidUpdate() {
+            console.log('[App.js] componentDidUpdate');
+        }
 
         state ={
         persons:[{name:'Priya',age:25,id:'abc'},{name:'Aman',age:25,id:'xyz'},{name:'Vidya',age:30,id:'mno'}],
-        showPerson: false
-    }
+        showPerson: false,
+        showCockpit:true,
+            changeCounter: 0
+        }
     switchNameHandler = (newName) =>{
         console.log(newName);
         this.setState({persons:[{name:newName,age:25}]});
@@ -34,12 +44,18 @@ import Cockpit from '../components/Cockpit/Cockpit';
         this.setState({persons:persons})
     }
     nameChangeHandler =(event,id)=>{
+            console.log('here in name change handler');
         const personIndex = this.state.persons.findIndex(p=>{return p.id===id});
         const person = {...this.state.persons[personIndex]};
         person.name = event.target.value;
         const persons =[...this.state.persons];
         persons[personIndex] = person;
-        this.setState({persons:persons});
+        this.setState((prevState,props)=>{
+            return ({
+                persons:persons,
+                changeCounter : prevState.changeCounter+ 1
+            }
+            )});
     }
 
     render (){
@@ -52,20 +68,21 @@ import Cockpit from '../components/Cockpit/Cockpit';
                 clicked={this.deletePersonHandler}
                 changed={this.nameChangeHandler}
             />
-
-
         }
 
 
     return(
-        <div className={classes.App}>
-            <Cockpit
-                persons={this.state.persons}
+        <Auxilary>
+
+            <button onClick={()=>{this.setState({showCockpit:false})}}>Remove cockpit</button>
+            {this.state.showCockpit?<Cockpit
+                personsLength={this.state.persons.length}
                 showPerson ={this.state.showPerson}
-                toggle ={this.togglePersonHandler}/>
+                toggle ={this.togglePersonHandler}/>:null}
             {persons}
-        </div>)
+        </Auxilary>
+    )
     }
 }
 
-export default App;
+export default withClass(App,classes.App);
