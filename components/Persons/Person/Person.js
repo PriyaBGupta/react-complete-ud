d81fe00827e10e55c80cac0d8de4d779746d1b9e
Priya Gupta <guptapriyab@gmail.com>
2019-09-26 23:44:01 +0200

    Added prop types

--- components/Persons/Person/Person.js
+++ components/Persons/Person/Person.js
@@ -1,10 +1,25 @@
-import React from 'react';
-const person = (props) =>{
-    return (<div>
-        <p onClick={props.click}> I am {props.name} and I am {props.age} years old!</p>
-        <p>{props.children}</p>
-        <input type="text" onChange={props.changed} value={props.name}/>
-    </div>)
+import React, {Component, Fragment} from 'react';
+import withClass from '../../../hoc/withClass';
+import Auxilary from '../../../hoc/Auxilary';
+import PropTypes from 'prop-types';
+class Person extends Component{
+
+    render(){
+        console.log('[Person.js] render');
+        return (
+            <Auxilary>
+                <p onClick={this.props.click} key='i1'> I am {this.props.name} and I am {this.props.age} years old!</p>
+                <p key='i2'>{this.props.children}</p>
+                <input key='i3' type="text" onChange={this.props.changed} value={this.props.name}/>
+            </Auxilary>
+        )
+    }
 }
-export default person;
+Person.propTypes={
+    click:PropTypes.func,
+    age:PropTypes.number,
+    name:PropTypes.string,
+    changed:PropTypes.func
+}
+export default withClass(Person);
 //Add a root elememt and wrap the transcluding element into that root element
