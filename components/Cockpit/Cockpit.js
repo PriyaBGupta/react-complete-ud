bdf4834dee80aeb6b3e9eb38f139c408df31d129
Priya Gupta <guptapriyab@gmail.com>
2019-10-08 15:47:55 +0200

    Added changes for context

--- components/Cockpit/Cockpit.js
+++ components/Cockpit/Cockpit.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React,{ useEffect,useRef } from 'react';
 import classes from './Cockpit.module.css';
-const cockpit = (props)=>{
+import AuthContext from '../../context/auth-context';
+const Cockpit = (props)=>{
+    const toggleBtnRef = useRef(null);
+    useEffect(()=>{
+        console.log('[[Cockpit.js] useEffect]');
+        /*setTimeout(()=>{
+            alert('saved data to cloud');
+
+        },1000);*/
+        toggleBtnRef.current.click();
+        return()=>{
+            console.log('[Cockpit] useEffect umount');
+
+        }
+    },[]);//if [] then no dependency and hence no repetition of call
+    console.log('[Cockpit.js] render');
     let style = classes['blue-button'];
     const assignedClasses =[];
-    if(props.persons.length<=2){
+    if(props.personsLength<=2){
         assignedClasses.push(classes['color-green']);
     }
-    if(props.persons.length<=1){
+    if(props.personsLength<=1){
         assignedClasses.push(classes.bold);
     }
     if(props.showPerson){
@@ -16,8 +31,11 @@ const cockpit = (props)=>{
         <div className={classes.Cockpit}>
             <h1>PI am React App</h1>
             <p className={assignedClasses.join(' ')}>Heyy I am working on classes</p>
-            <button className={style} onClick={props.toggle}>togglePerson</button>
+            <button ref= {toggleBtnRef} className={style} onClick={props.toggle}>togglePerson</button>
+            <AuthContext.Consumer>
+                {context => <button onClick={context.login}>Login</button>}
+            </AuthContext.Consumer>
         </div>
     )
 }
-export default cockpit;
+export default React.memo(Cockpit);
