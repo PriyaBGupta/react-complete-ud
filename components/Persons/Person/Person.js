c950aa11f8c9c2aacc55ecb6e04749f6363ee18e
Priya Gupta <guptapriyab@gmail.com>
2019-10-08 15:48:59 +0200

    Added context changes in Person.js

--- components/Persons/Person/Person.js
+++ components/Persons/Person/Person.js
@@ -1,10 +1,36 @@
-import React from 'react';
-const person = (props) =>{
-    return (<div>
-        <p onClick={props.click}> I am {props.name} and I am {props.age} years old!</p>
-        <p>{props.children}</p>
-        <input type="text" onChange={props.changed} value={props.name}/>
-    </div>)
+import React, {Component, Fragment} from 'react';
+import withClass from '../../../hoc/withClass';
+import Auxilary from '../../../hoc/Auxilary';
+import PropTypes from 'prop-types';
+import AuthContext from '../../../context/auth-context';
+class Person extends Component{
+    constructor(props){
+        super(props);
+        this.inputElement = React.createRef();
+    }
+    componentDidMount() {
+        this.inputElement.current.focus();
+    }
+
+    render(){
+        return (
+            <Auxilary>
+                <AuthContext.Consumer>
+                    {context => context.authenticated?<p>Authenticated!</p>:<p>Please login</p>}
+                </AuthContext.Consumer>
+                <p onClick={this.props.click} key='i1'> I am {this.props.name} and I am {this.props.age} years old!</p>
+                <p key='i2'>{this.props.children}</p>
+                <input key='i3' type="text" onChange={this.props.changed} value={this.props.name}
+                ref={this.inputElement}/>
+            </Auxilary>
+        )
+    }
 }
-export default person;
+Person.propTypes={
+    click:PropTypes.func,
+    age:PropTypes.number,
+    name:PropTypes.string,
+    changed:PropTypes.func
+}
+export default withClass(Person);
 //Add a root elememt and wrap the transcluding element into that root element
